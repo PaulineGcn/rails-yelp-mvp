e5e46dd007b31f54333303bc849e3f17b8433e66
Pauline Garcin <paulinegarcin28@gmail.com>
2018-11-21 18:32:35 +0100

    reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Mugs',
+    address:      'Avenue de Valescure, Saint Raphael',
+    phone_number: '0494409874',
+    category:     'french',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '0494409878',
+    category:     'italian',
+  },
+  {
+    name:         'Alicheur',
+    address:      '7 Rue Oberkampf, Paris 75011',
+    phone_number: '0494409888',
+    category:     'japanese',
+  },
+  {
+    name:         'Ober Mama',
+    address:      '300 rue saint-maur, Paris 75011',
+    phone_number: '0494409864',
+    category:     'italian',
+  },
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '0494409854',
+    category:     'japanese',
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
